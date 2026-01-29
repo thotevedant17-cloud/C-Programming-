42f375c7a458d68b047237893b08542113bcbd94
thotevedant17-cloud <thotevedant17@gmail.com>
2026-01-29 10:21:12 +0530

    Add C program to compute matrix transpose

---
+++ 2. Arrays and Strings/2.1/2.1.3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+   
+    int n;
+	int matrix[100][100],transpose[100][100];
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			transpose[j][i] = matrix[i][j];
+		}
+	}
+	for (int i=0;i <n;i++){
+		for (int j=0;j<n;j++){
+			printf("%d ", transpose[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
